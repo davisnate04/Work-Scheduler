ac75a73596e7870349ad58010253f8f42e1e097d
davisnate04 <111401054+davisnate04@users.noreply.github.com>
2024-01-03 15:09:27 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,27 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 $(function () {
   const hours = JSON.parse(localStorage.getItem("hours")) || [];
   const timeBlock = document.getElementsByClassName("time-block");
   const currentDay = document.getElementById("currentDay");
   const timeArray = ["past", "present", "future"];
   const day = dayjs();
+  let create = false;
   let intervalStart;
 
 
   currentDay.innerHTML = day.format('dddd, MMMM D')
+  // Creates the element only once
+  function createElement() {
+      if (!create) {
+      const appointment = document.createElement('p');
 
+      appointment.innerHTML = `Appointment Added to <span style="color: #AA336A; font-weight:bold;">localStorage</span> &#10003`;
+      appointment.setAttribute("class", "text-center");
+      appointment.setAttribute("style", "position: relative; top: 75px; font-weight:bold; border-top: 8px solid black");
+      document.body.children[0].appendChild(appointment);
+
+      create = true;
+      }
+  
   // Saves the text next to the save button
   function save(event) {
     if (event.target.ariaLabel === "save") {
